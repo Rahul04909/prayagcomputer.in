f7bd9b8c9f556e0ee8513b7e9197893dd39c1f74
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-03-12 18:19:50 +0530

    updated admin panel

--- admin/header.php
+++ admin/header.php
@@ -9,7 +9,7 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="assets/css/admin-style.css">
+    <link rel="stylesheet" href="assets/css/admin-style.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <div class="admin-container">
